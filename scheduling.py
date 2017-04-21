62f7f299304fc943b9f7bf97ebc069a13808d5e9
Douglas Strodtman <dstrodtman@gmail.com>
2017-04-21 12:57:44 -0700

    Updated documentation in sched

--- scheduling.py
+++ scheduling.py
@@ -1,3 +1,5 @@
+# This script requires reports "Scheduling" to be downloaded most recently from Repo and Reg. Generates a file 'sched.csv' to import into reg.
+
 import pandas as pd
 import numpy as np
 import os
@@ -15,21 +17,23 @@ reg = pd.read_csv(reg_file, index_col='record_id', dtype=object)
 repo = pd.read_csv(repo_file, index_col='record_id', dtype=object)
 
 # import time and get current time
-
 from time import gmtime, strftime
 now = strftime("%Y-%m-%d %H:%M", gmtime())
 
 # find all subs with upcoming appointments
 upcoming = pd.unique(repo.loc[repo['visit_dt'] >= now].index)
 
+# change recruitment and past sub status of those without upcoming appointments
 for sub in reg.index:
     if not sub in upcoming:
         if reg.ix[sub, 'recruiting_status'] == '2':
             reg.ix[sub, 'recruiting_status'] = 0
             reg.ix[sub, 'past_sub'] = 1
-             
+
+# set subs with upcoming appointments as scheduled             
 for sub in upcoming:
     reg.loc[reg.index == sub, 'recruiting_status']=2
     reg.loc[reg.index == sub, 'past_sub'] = 1
-    
+   
+# write out csv for upload to registry 
 reg.to_csv(home+'/Downloads/sched.csv')
